d56b5396a5a7b0dca3b33f50fe4feb8553d7b8e7
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-04-02 01:40:34 +0530

    Add Day One Operators exercises: arithmetic operations with practical applications

---
+++ src/DayOne/Operators/TriangleArea.java
@@ -0,0 +1,16 @@
+package DayOne.Operators;
+
+import java.util.Scanner;
+public class TriangleArea {
+    public static void main(String[] args) {
+        // Area of triangle = ½*B*H
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sides of triangle: ");
+        double b = sc.nextDouble();
+        double h = sc.nextDouble();
+
+        double area = (1.0/2.0) * b * h;
+        System.out.println("Area of triangle is: " + area);
+        sc.close();
+    }
+}
